6a898c08b3cbf37cbf1289038792c5f724d0c468
JuTheCutie <joelalexandreff@hotmail.com>
2020-09-24 17:12:58 +0100

    update

---
+++ client/src/components/wecome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main">
+        <h1>Welcome {{ user.name }}</h1>
+        <h2>Today it's {{ date }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
