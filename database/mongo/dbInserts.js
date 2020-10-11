20de84c34fe45d958fb9ad2e9ce82ac63a8eb253
JuTheCutie <joelalexandreff@hotmail.com>
2020-10-11 16:57:39 +0100

    update

--- database/mongo/dbInserts.js
+++ database/mongo/dbInserts.js
@@ -2,10 +2,8 @@
 db.color.insertMany([
     { name: "AliceBlue", hexCode: "#F0F8FF" },
     { name: "Aqua", hexCode: "#00FFFF" },
-    { name: "Black", hexCode: "#000000" },
     { name: "BlueViolet", hexCode: "#8A2BE2" },
     { name: "Brown", hexCode: "#A52A2A" },
-    { name: "DarkViolet", hexCode: "#9400D3" },
     { name: "DeepPink", hexCode: "#FF1493" },
 ]);
 
